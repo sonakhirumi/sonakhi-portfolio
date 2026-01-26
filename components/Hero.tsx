b5cdafe144475f2e2923b9e762fedb970b215cef
sonakhirumi <sonakhirumi@gmail.com>
2026-01-26 19:56:22 +0530

    hero title alignment

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -53,8 +53,8 @@ const Hero: React.FC = () => {
             </div>
 
             <h1 className="font-serif text-6xl md:text-8xl lg:text-[7rem] font-medium text-stone-900 leading-[0.95] tracking-tight animate-in fade-in slide-in-from-left-8 duration-1000">
-              Let me <span className="italic font-light text-stone-400">help</span> you <br />
-              find the <span className="relative">
+              Let me <span className="italic font-light text-stone-400">help</span> you find <br />
+              the <span className="relative">
                 right words.
                 <svg className="absolute -bottom-2 left-0 w-full h-3 text-stone-200 -z-10 opacity-60" viewBox="0 0 100 10" preserveAspectRatio="none">
                   <path d="M0 5 Q 25 0 50 5 T 100 5" fill="none" stroke="currentColor" strokeWidth="4" />
